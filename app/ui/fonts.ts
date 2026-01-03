c64fd39184100c49b969bf7f7a6d5a95966b9d4a
shashwa7-dev <shashwa7.dev@gmail.com>
2026-01-03 18:15:41 +0530

    build fix

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,11 @@
-import { Poppins } from "next/font/google";
+import { Poppins, Lusitana } from "next/font/google";
 
 export const inter = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
 });
