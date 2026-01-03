74d6fd3d7e1025ce8a25b1b3d4d625977170a141
shashwa7-dev <shashwa7.dev@gmail.com>
2026-01-03 17:47:51 +0530

    initial commit

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,5 @@
+const Loading = () => {
+  return <div>Loading dashboard</div>;
+};
+
+export default Loading;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Dashboar Page</div>;
+};
+
+export default Page;
